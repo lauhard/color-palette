5a871dae87c827ee6329ba62002b582ff2aaecbd
Andreas Lauhard <lauhard@proton.me>
2026-03-06 00:37:03 +0100

    chore: color-box logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,28 @@
 <script lang="ts">
     import Color, { type ColorInstance } from "color";
     import ColorBox, { type ColorBoxEntity } from "./ColorBox.svelte";
-    let paletteName = $state("");
+    import { LucideCirclePlus, LucidePen } from "@lucide/svelte";
+    let paletteName = $state("First Palette");
     const addPalette = (e: SubmitEvent) => {
         e.preventDefault();
     };
-    
-    let color= $state("#ffff");
-    let col = $derived<ColorInstance>(Color(color))
-   
-    let colorPalette =$state<ColorBoxEntity[]>([]);
 
-    const addColorBox=()=>{
+    let color = $state("#66606");
+
+    let colorPalette = $state<ColorBoxEntity[]>([]);
+
+    const addColorBox = () => {
         const id = crypto.randomUUID();
-        let colorBox:ColorBoxEntity={
+        let colorBox: ColorBoxEntity = {
             name: "some name",
-            id:id,
-            color: colorPalette.length > 0 ? colorPalette[colorPalette.length-1].color : "#ffffff"
-        }
-        colorPalette=[...colorPalette, colorBox];
-    }
-    //add a div as a grid to the page
+            id: id,
+            color:
+                colorPalette.length > 0
+                    ? colorPalette[colorPalette.length - 1].color
+                    : "#ffffff",
+        };
+        colorPalette = [...colorPalette, colorBox];
+    };
 </script>
 
 <form onsubmit={addPalette}>
@@ -29,14 +31,26 @@
 </form>
 
 <div class="palette-container">
-    <div class="palette">
-        {#each colorPalette as colorBox}
-             <!-- content here -->
-            <ColorBox {colorBox}></ColorBox>
-        {/each}
-        <button class="btn" onclick={addColorBox}>add color</button>
+    <div class="palette" style="--palette-color:{color}">
+        <div class="palette-header">
+            <div>
+                <input type="text" bind:value={paletteName} />
+                <LucidePen></LucidePen>
+            </div>
+            <div>
+                <input type="color" bind:value={color} name={color} />
+                <LucidePen></LucidePen>
+            </div>
+        </div>
+        <div class="palette-color-boxes">
+            {#each colorPalette as colorBox, i}
+                <ColorBox bind:colorBox={colorPalette[i]}></ColorBox>
+            {/each}
+            <button class="btn" onclick={addColorBox}
+                ><LucideCirclePlus size="2rem"></LucideCirclePlus>
+            </button>
+        </div>
     </div>
-    
 </div>
 
 <style>
@@ -47,15 +61,36 @@
         background-color: aliceblue;
         display: flex;
         flex-direction: column;
-        gap: 1rem;
     }
     .palette {
-        display: grid;
-        grid-template-columns: repeat(7, minmax(120px, 1fr));
-        background-color: rgb(223, 232, 232);
-        gap: 1rem;
-        border-radius: 10px;
-        min-height: 140px;
+        background-color: var(--palette-color);
+        width: 100%;
+        height: auto;
+        display: flex;
+        flex-direction: column;
     }
-   
+    .palette-color-boxes {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
+        border-radius: 10px;
+        min-height: 130px;
+        padding-inline: 1rem;
+        padding-block: 1rem;
+        row-gap: 1rem;
+        column-gap: 0.5rem;
+        align-content: center;
+        border-radius: 20px;
+    }
+    .btn {
+        outline: transparent;
+        border: transparent;
+        width: 120px;
+        height: 120px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: wheat;
+        background-color: #4a63b0;
+        border-radius: 20px;
+    }
 </style>
--- src/routes/ColorBox.svelte
+++ src/routes/ColorBox.svelte
@@ -5,16 +5,21 @@
         color: string;
     };
     import Color, { type ColorInstance } from "color";
-    let { colorBox=$bindable() }: { colorBox: ColorBoxEntity }=$props();
-    let color = $state(colorBox.color);
-    
-    
-    
-    let col = $derived<ColorInstance>(Color(color));
+    let { colorBox = $bindable() }: { colorBox: ColorBoxEntity } = $props();
+    let col = $derived<ColorInstance>(Color(colorBox.color));
 </script>
 
-<div class=" color-box" data-color="#565656">
-    <input type="color" bind:value={colorBox.color} name="#565656" id={colorBox.id} />
+<div
+    class=" color-box"
+    data-color={colorBox.color}
+    style="--color:{colorBox.color}"
+>
+    <input
+        type="color"
+        bind:value={colorBox.color}
+        name={colorBox.color}
+        id={colorBox.id}
+    />
     <input
         type="text"
         bind:value={colorBox.color}
@@ -25,16 +30,38 @@
 
 <style>
     .color-box {
-        /* background-color: attr(data-color type(<color>), white); */
         width: inherit;
         height: auto;
+        max-width: 120px;
+        max-height: 120px;
         aspect-ratio: 1 / 1;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        border-radius: 10px;
-        overflow: hidden;
+        box-sizing: border-box;
+
+        background-image:
+            radial-gradient(circle at 50% 250%, var(--color), var(--color)),
+            linear-gradient(
+                90deg,
+                color-mix(in oklab, var(--color) 100%, white 90%),
+                var(--color)
+            );
+        background-origin: padding-box, border-box;
+        background-clip: padding-box, border-box;
+        border: var(--color) 2px solid;
+        corner-shape: squircle;
+        border-radius: 90px;
+        overflow: clip;
+        &:hover {
+            box-shadow: 2.5px 1px 4px 0.5px rgb(41, 40, 40);
+            transform: scale(1.02);
+            border: transparent 2px solid;
+        }
+        transition-property: box-shadow, transform, border;
+        transition-duration: 200ms;
+        transition-timing-function: ease-in-out;
 
         input[type="color"] {
             height: 100%;
@@ -46,17 +73,14 @@
             outline: none;
             inset: none;
             border-style: none;
-            border-color: transparent;
         }
         input[type="color"]::-moz-color-swatch {
             border: none;
         }
-
         input[type="color"]::-webkit-color-swatch-wrapper {
             padding: 0;
             border-radius: 0;
         }
-
         input[type="color"]::-webkit-color-swatch {
             border: none;
         }
@@ -68,17 +92,12 @@
             text-align: center;
             font-weight: bold;
             letter-spacing: 2px;
-            background-color: white;
+            background-color: #363232a9;
             border: transparent;
             padding-block: 0.5rem;
             font-size: 1rem;
-            color: black;
-            padding-inline: 5px;
+            color: rgb(245, 240, 240);
             border-radius: 10px;
-        }
-        .dark {
-            background-color: black;
-            color: white;
         }
     }
 </style>
